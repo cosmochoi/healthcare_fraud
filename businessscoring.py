8b7b3b09b77eb7fd6a3e2fe51faab1ae43b6ba22
princegauss411 <ptruongnp@gmail.com>
2020-11-22 10:46:55 -0500

    new business scoring function

--- businessscoring.py
+++ businessscoring.py
@@ -3,4 +3,4 @@ def businessgain(ytest,ypredict):
     TP = confusion_matrix(ytest,ypredict)[1,1]
     FP = confusion_matrix(ytest,ypredict)[0,1]
     FN = confusion_matrix(ytest,ypredict)[1,0]
-    return (1498*(TP-FN)-8*20*(FP+TP))/((1498-8*20)*(TP+FN)) #8-hour investigation at $20/h 
+    return (1876.7*(TP-FN)-8*20*(FP+TP))/((1876.7-8*20)*(TP+FN)) #8-hour investigation at $20/h 
